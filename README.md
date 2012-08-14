43b410bc95866811313cb7bdfe67787b7102e6fb
Pere Villega <pere.villega@gmail.com>
2012-08-14 20:55:04 +0100

    First commit, added core project files

--- README.md
+++
@@ -1,4 +0,0 @@
-play-samples-javascript
-=======================
-
-Set of samples, mainly of Javascript libraries and UI customizations, using Play 2.0
---
+++ public/javascripts/app.js
@@ -0,0 +1,24 @@
+/** CUSTOM SCRIPTS HERE */
+/**
+* Sets the proper selected option in the navbar
+*/
+function setNavbar(id) {
+    //remove all selectors
+    $('#menuHome').removeClass('active');
+
+    //set given one
+    $("#menu"+id).addClass('active');
+}
+
+/** END OF CUSTOM SCRIPTS */
+
+/*!
+* Below there are core functional scripts added in here to load less files on request, don't change
+*/
+/*!
+* Allows $(function() {}); to be used even without Jquery loaded - This code fragments runs the stored calls.
+* This is useful so we can push all the $(function() { ...}); calls in templates without having to load jquery at head
+*/window.$.noConflict();window.$ = window.$.attachReady(jQuery);
+/*!
+* Prevention of window hijack, run after all jquery scripts
+*/$('html').css('display','none');if( self == top ){document.documentElement.style.display = 'block';}else{top.location = self.location;}
